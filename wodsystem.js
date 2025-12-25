e67d7601d929966d4b056399f9f5550789059a4a
Jhonathan Ibanez <jhonathan.ibanez.g@gmail.com>
2025-12-25 04:05:25 -0500

    fix: Remove deleted template from preload list in wodsystem.js
    - Remove quick-rolls-panel.html from loadTemplates array
    - Fixes template loading error that broke character sheet rendering

--- wodsystem.js
+++ wodsystem.js
@@ -42,7 +42,6 @@ Hooks.once("init", async function() {
         "systems/wodsystem/templates/actor/partials/advantages-common.html",
         "systems/wodsystem/templates/actor/partials/mortal-numina.html",
         "systems/wodsystem/templates/actor/partials/experience.html",
-        "systems/wodsystem/templates/actor/partials/quick-rolls-panel.html",
         "systems/wodsystem/templates/apps/roll-dialog.html",
         "systems/wodsystem/templates/dice/roll-card.html"
     ]);
